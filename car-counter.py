74cce66807a39ecd209e63524d4b4f8890a68704
Renny <rennyemmanuel8@gmail.com>
2024-01-10 00:01:19 -1200

    worked on: confidence

--- car-counter.py
+++ car-counter.py
@@ -60,4 +60,7 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             w, h = (x2-x1), (y2-y1)
 
+            # Confidence
+            conf = math.floor(box.conf[0]*100)/100
+
 
